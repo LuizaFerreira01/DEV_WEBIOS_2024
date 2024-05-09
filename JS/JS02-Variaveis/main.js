839a235ae4aaac4fe7f6e4de176220af24c1a432
LuizaFerreira01 <luiza05fs@gmail.com>
2024-05-08 21:45:24 -0300

    javascript

---
+++ JS/JS02-Variaveis/main.js
@@ -0,0 +1,58 @@
+// objeto console
+console.log("Hello World!")
+
+// Quando trabalho com texto coloco o mesmo dentro de aspas duplas ou simples 
+
+// Variaveis
+// Quero criar um computador chamdo _felippe e dizer que esse espaço na memória irá receber o valor 8
+
+var _felippe = 8
+var _felippe = "Messi" // Redeclarar uma variavel
+
+let letsGo = 4  // Não posso redeclarar, mas posso trocar os valores
+letsGo = 6
+
+console.log(letsGo)
+
+console.clear()
+
+// Const
+// Não pode ser redeclarado e não pode alterar os valores 
+const PI = 3.1415 
+
+console.log(PI) 
+
+console.clear()
+
+let NAN = 0/0
+console.log(NAN)
+
+console.clear()
+let corinthians = "melhor Time, palmeiras sem mundial"
+console.log(corinthians)
+
+console.clear()
+let IOS = "instituto" + "da" + "oprtunidade" + "social" // Variavel IOS recebeu uma string
+console.log(IOS)
+
+var numero1 = 2
+var texto = "Eu tenho"
+var texto2 = "Reais"
+
+console.log (texto + numero1 + texto2)
+// Concatear = é possivel concatear Strings ou concatear variaveis
+
+console.clear()
+
+//Valores Booleanos
+
+var num1 = 1
+var num2 = 1
+// num é igual a num2
+// 1 é igual a 1 = true (verdadeiro)
+
+console.log (num1 == num2)
+
+var teste
+
+console.log()
